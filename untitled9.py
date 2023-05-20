256f5438ab9185d5a20f4d8b8f589dd80787fda0
Zorro1951 <134135982+Zorro1951@users.noreply.github.com>
2023-05-20 17:50:57 +0300

    Update untitled9.py

--- untitled9.py
+++ untitled9.py
@@ -1,5 +1,3 @@
-
-
 import math
 import matplotlib.pyplot as plt
 import numpy as np
@@ -21,7 +19,7 @@ class Fall(object):
     return (2 * (np.sqrt(2 * self.__h0 / Fall.g))) + ((self.__v0 * np.sin(Fall.alpha)) / Fall.g)
 
 fall = Fall(h0, v0)
-t_max = fall.max_time()
+t_max = fall.min_time()
 
 t = np.linspace(0, t_max , 1000)
 x = fall.x(t)
@@ -42,5 +40,5 @@ R = 8.31
 g = 9.81
 k = 1.38 * 10**(-23)
 p = p0 * math.exp(-(M* g * H) / (R * T))
-N = (p * V) / (k * T)
+N = (p * V) * (k * T)
 print("p =", p, "N =", N)
