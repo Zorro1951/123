a7001eda9f70a36c9dda63ad8b2dba3eedbf2e21
Zorro1951 <134135982+Zorro1951@users.noreply.github.com>
2023-05-20 17:54:19 +0300

    Update untitled9.py

--- untitled9.py
+++ untitled9.py
@@ -12,33 +12,21 @@ class Fall(object):
     self.__h0 = h0
     self.__v0 = v0
   def y(self, t):
-    return self.__h0 + self.__v0* np.sin(Fall.alpha) * t - (Fall.g * (t**2))/2
+    return self.__h0 + self.__v0* np.cos(Fall.alpha) * t - (Fall.g * (t**2))/2
   def x(self, t):
-    return self.__v0 * np.cos(Fall.alpha) * t
+    return self.__v0 * np.sin(Fall.alpha) * t
   def max_time(self):
     return (2 * (np.sqrt(2 * self.__h0 / Fall.g))) + ((self.__v0 * np.sin(Fall.alpha)) / Fall.g)
 
 fall = Fall(h0, v0)
 t_max = fall.min_time()
 
-t = np.linspace(0, t_max , 1000)
+t = np.linspace(0, t_min , 100)
 x = fall.x(t)
 y = fall.y(t)
 
-plt.plot(x, y, color='green', linewidth=2)
+plt.plot(x, y, color='red', linewidth=6)
 plt.ylim(bottom = 0)
-plt.xlim(left = 0)
+plt.xlim(right = 0)
 plt.legend(loc='upper right', fontsize=27, title='Движение тела')
 plt.grid(True)
-
-H = float(input ('H = '))
-V = float(input('V = '))
-T = float(input('T = '))
-M = 32 * 10**(-3)
-p0 = 10**5
-R = 8.31
-g = 9.81
-k = 1.38 * 10**(-23)
-p = p0 * math.exp(-(M* g * H) / (R * T))
-N = (p * V) * (k * T)
-print("p =", p, "N =", N)
